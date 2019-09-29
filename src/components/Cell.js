b12619b7af2b75452698a7e8802a0ab2de4080ec
Павел <elantsev.pa@gmail.com>
2019-09-29 22:47:16 +0300

    Styling with Styled Components

---
+++ src/components/Cell.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "./../tetrominos";
+
+const Cell = ({ type }) => (
+  <StyledCell type={'T'} color={TETROMINOS['T'].color}>
+    cell
+  </StyledCell>
+);
+
+export default Cell;
