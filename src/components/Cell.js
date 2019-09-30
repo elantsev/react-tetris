20186f5c1350580aa79a59f0ce11c0ab9d2e0476
Павел <elantsev.pa@gmail.com>
2019-09-30 11:08:35 +0300

    +usePlayer and useStage

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,7 @@ import { StyledCell } from "./styles/StyledCell";
 import { TETROMINOS } from "./../tetrominos";
 
 const Cell = ({ type }) => (
-  <StyledCell type={"T"} color={TETROMINOS["T"].color} />
+  <StyledCell type={type} color={TETROMINOS[type].color} />
 );
 
 export default Cell;
